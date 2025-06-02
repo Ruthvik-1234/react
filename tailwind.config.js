d6c7649a8f83ff974ab6e8af1dada398f022e60a
Ruthvik-1234 <160007463+Ruthvik-1234@users.noreply.github.com>
2025-06-02 17:29:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme.js';
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['ClashDisplay-Regular', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        tomato: '#E50914',
+        marigold: '#ffbe0b',
+      },
+    },
+
+    },
+  
+  plugins: [],
+}
+
